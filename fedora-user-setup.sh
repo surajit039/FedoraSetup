741d43b1aa465fb18857464d8201251c6e595f4e
surajit039 <57254249+surajit039@users.noreply.github.com>
2021-04-14 02:58:32 -0400

    Add files via upload

---
+++ fedora-user-setup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+mkdir -p ~/.cache/wget
+cd ~/.cache/wget
+wget https://noto-website-2.storage.googleapis.com/pkgs/Noto-unhinted.zip
+unzip Noto-unhinted.zip
+mkdir -p ~/.fonts
+cp -fr *ttf ~/.fonts/
+
+gsettings set org.gnome.desktop.interface gtk-theme Yaru-light
+gsettings set org.gnome.desktop.sound theme-name Yaru
+gsettings set org.gnome.desktop.interface icon-theme Yaru
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+/usr/bin/canberra-gtk-play --id="desktop-login" --description="GNOME Login"
