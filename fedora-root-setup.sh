11d374dbcdb9a7dea50305052b8e252b6ba950ab
surajit039 <57254249+surajit039@users.noreply.github.com>
2021-04-28 09:31:10 +0530

    Update fedora-root-setup.sh

--- fedora-root-setup.sh
+++ fedora-root-setup.sh
@@ -6,6 +6,8 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 
 sudo dnf install -y vlc gnome-sound-recorder transmission obs-studio gnome-sound-recorder firefox mozilla-ublock-origin chromium yaru-theme gnome-tweaks gnome-extensions-app gimp pitivi gstreamer1* youtube-dl clipgrab gparted python python2 python3 make cmake automake autoconf gcc g++ adb java-latest-openjdk-devel procyon-decompiler meld ghex gnupg brasero audacity ibus-m17n hunspell-as hunspell-bn hunspell-en hunspell-gu hunspell-hi hunspell-kn hunspell-ml hunspell-mr hunspell-ne hunspell-or hunspell-pa hunspell-ta hunspell-te hyphen-sa gnome-multi-writer seahorse wget git evince-nautilus brasero-nautilus easytag-nautilus gtkhash-nautilus seahorse-nautilus evolution gvfs-mtp wireshark perl openssl openjfx-devel nodejs npm nautilus-extensions golang  tesseract tesseract-langpack-asm tesseract-langpack-chi_sim  tesseract-langpack-chi_tra  tesseract-langpack-eng tesseract-langpack-guj  tesseract-langpack-hin tesseract-langpack-kan tesseract-langpack-mal tesseract-langpack-mar tesseract-langpack-mya tesseract-langpack-nep tesseract-langpack-ori tesseract-langpack-pan tesseract-langpack-san tesseract-langpack-tam btrfs-progs e2fsprogs f2fs-tools dosfstools mtools hfsutils jfsutils util-linux cryptsetup lvm2 nilfs-utils reiserfs-utils udftools xfsprogs xfsdump qt5ct inkscape timeshift
 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 sudo flatpak install -y flathub org.standardnotes.standardnotes
 
 sudo dnf update -y
