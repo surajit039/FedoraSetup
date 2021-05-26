1e86f8a5910508f6a6a298db330d6a8bf1a7903b
surajit039 <57254249+surajit039@users.noreply.github.com>
2021-05-26 19:56:04 +0530

    Update fedora-user-setup.sh

--- fedora-user-setup.sh
+++ fedora-user-setup.sh
@@ -13,4 +13,13 @@ gsettings set org.gnome.desktop.sound theme-name Yaru
 gsettings set org.gnome.desktop.interface icon-theme Yaru
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 gsettings set org.gnome.desktop.interface enable-hot-corners false
+gnome-extensions enable dash-to-dock@micxgx.gmail.com
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-overview'
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-background-color true
+gsettings set org.gnome.shell.extensions.dash-to-dock background-color '#62554A'
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 32
+gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 /usr/bin/canberra-gtk-play --id="desktop-login" --description="GNOME Login"
