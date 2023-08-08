91a82e033dc2bbd45d6722fb059dc9ba40cf2d77
surajit039 <57254249+surajit039@users.noreply.github.com>
2023-08-08 17:56:59 +0530

    Update For-Indians.sh

--- For-Indians.sh
+++ For-Indians.sh
@@ -5,7 +5,7 @@ pkexec bash -c "
 
 dnf remove -y adwaita-qt6 adwaita-qt5 totem gnome-calendar epiphany evolution lohit-assamese-fonts lohit-bengali-fonts lohit-devanagari-fonts lohit-gujarati-fonts lohit-gurmukhi-fonts lohit-kannada-fonts lohit-malayalam-fonts lohit-marathi-fonts lohit-nepali-fonts lohit-odia-fonts lohit-tamil-classical-fonts lohit-tamil-fonts lohit-telugu-fonts google-noto-cjk-fonts-common google-noto-emoji-color-fonts google-noto-sans-cjk-ttc-fonts google-noto-sans-gurmukhi-fonts google-noto-sans-sinhala-vf-fonts bitstream-vera* paktype* java-11-openjdk java-11-openjdk-devel gnome-shell-extension-window-list gnome-shell-extension-places-menu gnome-shell-extension-background-logo gnome-contacts gnome-weather gnome-maps gnome-photos mediawriter libreoffice-calc libreoffice-writer libreoffice-impress rhythmbox gnome-tour firefox &&
 
-dnf install -y gnome-tweaks file-roller gnome-shell-extension-appindicator gnome-shell-extension-gsconnect fondo transmission gnome-sound-recorder gparted python make cmake automake autoconf gcc g++ adb java-latest-openjdk-devel procyon-decompiler meld ghex gnupg brasero seahorse wget git evince-nautilus brasero-nautilus gtkhash-nautilus nautilus-image-converter seahorse-nautilus gvfs-mtp wireshark openssl nodejs npm nautilus-extensions btrfs-progs exfatprogs e2fsprogs f2fs-tools dosfstools mtools hfsutils jfsutils util-linux cryptsetup lvm2 nilfs-utils udftools xfsprogs xfsdump &&
+dnf install -y qbittorent gnome-tweaks file-roller gnome-shell-extension-appindicator gnome-shell-extension-gsconnect fondo gnome-sound-recorder gparted python make cmake automake autoconf gcc g++ adb java-latest-openjdk-devel procyon-decompiler meld ghex gnupg brasero seahorse wget git evince-nautilus brasero-nautilus gtkhash-nautilus nautilus-image-converter seahorse-nautilus gvfs-mtp wireshark openssl nodejs npm nautilus-extensions btrfs-progs exfatprogs e2fsprogs f2fs-tools dosfstools mtools hfsutils jfsutils util-linux cryptsetup lvm2 nilfs-utils udftools xfsprogs xfsdump &&
 
 dnf clean packages &&
 
@@ -17,7 +17,7 @@ flatpak remote-modify --enable flathub &&
 
 flatpak mask org.kde.KStyle.Adwaita &&
 
-flatpak install flathub com.mattjakeman.ExtensionManager com.google.Chrome org.videolan.VLC &&
+flatpak install flathub com.mattjakeman.ExtensionManager com.google.Chrome org.videolan.VLC com.visualstudio.code &&
 
 alternatives --set java java-latest-openjdk.x86_64"
 
