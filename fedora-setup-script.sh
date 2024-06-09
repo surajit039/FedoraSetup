92c83bd50d3b8e7b47e4fa2c4e7d6c50830d7e57
surajit039 <57254249+surajit039@users.noreply.github.com>
2024-06-09 14:33:45 +0530

    Update fedora-setup-script.sh

--- fedora-setup-script.sh
+++ fedora-setup-script.sh
@@ -5,7 +5,7 @@ pkexec bash -c "
 
 dnf remove -y adwaita-qt6 adwaita-qt5 totem gnome-calendar epiphany evolution java-17-openjdk java-17-openjdk-devel gnome-shell-extension-window-list gnome-shell-extension-places-menu gnome-shell-extension-background-logo gnome-contacts gnome-weather gnome-maps gnome-photos mediawriter libreoffice-calc libreoffice-writer libreoffice-impress rhythmbox gnome-tour firefox &&
 
-dnf install -y gnome-tweaks file-roller p7zip-plugins p7zip gnome-shell-extension-appindicator gnome-shell-extension-gsconnect gnome-shell-extension-caffeine gnome-shell-extension-blur-my-shell fondo gnome-sound-recorder gparted python make cmake automake autoconf gcc g++ adb java-latest-openjdk-devel procyon-decompiler meld ghex gnupg brasero seahorse wget git seahorse-nautilus gvfs-mtp wireshark openssl nodejs npm nautilus-extensions btrfs-progs exfatprogs e2fsprogs f2fs-tools dosfstools mtools hfsutils jfsutils util-linux cryptsetup lvm2 nilfs-utils udftools xfsprogs xfsdump &&
+dnf install -y gnome-tweaks file-roller fragments p7zip-plugins p7zip gnome-shell-extension-appindicator gnome-shell-extension-dash-to-dock gnome-shell-extension-netspeed gnome-shell-extension-user-theme gnome-shell-extension-caffeine gnome-shell-extension-blur-my-shell la-capitaine-cursor-theme fondo gnome-sound-recorder gparted python make cmake automake autoconf gcc g++ adb java-latest-openjdk-devel procyon-decompiler meld ghex gnupg brasero seahorse wget git seahorse-nautilus gvfs-mtp wireshark openssl nodejs npm nautilus-extensions btrfs-progs exfatprogs e2fsprogs f2fs-tools dosfstools mtools hfsutils jfsutils util-linux cryptsetup lvm2 nilfs-utils udftools xfsprogs xfsdump &&
 
 dnf clean packages &&
 
@@ -17,7 +17,7 @@ flatpak remote-modify --enable flathub &&
 
 flatpak mask org.kde.KStyle.Adwaita &&
 
-flatpak install flathub -y com.mattjakeman.ExtensionManager com.google.Chrome org.mozilla.firefox org.qbittorrent.qBittorrent org.videolan.VLC com.visualstudio.code org.onlyoffice.desktopeditors &&
+flatpak install flathub -y com.mattjakeman.ExtensionManager com.google.Chrome org.mozilla.firefox org.videolan.VLC com.visualstudio.code org.onlyoffice.desktopeditors &&
 
 alternatives --set java java-latest-openjdk.x86_64 &&
 
@@ -43,8 +43,8 @@ wget https://dtinth.github.io/comic-mono-font/ComicMono.ttf"
 
 fc-cache -v -f
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
-gsettings set org.gnome.shell enabled-extensions "['appindicatorsupport@rgcjonas.gmail.com', 'gsconnect@andyholmes.github.io', 'blur-my-shell@aunetx', 'caffeine@patapon.info']"
-gsettings set org.gnome.desktop.interface document-font-name 'SF Pro Display 11'
-gsettings set org.gnome.desktop.interface font-name 'SF Pro Display 11'
+gsettings set org.gnome.shell enabled-extensions "['appindicatorsupport@rgcjonas.gmail.com', 'dash-to-dock@micxgx.gmail.com', 'blur-my-shell@aunetx', 'caffeine@patapon.info', 'netspeed@hedayaty.gmail.com', 'user-theme@gnome-shell-extensions.gcampax.github.com']"
+gsettings set org.gnome.desktop.interface document-font-name 'BlinkMacSystemFont 11'
+gsettings set org.gnome.desktop.interface font-name 'BlinkMacSystemFont Medium 11'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Comic Mono 10'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'SF Pro Display Semi-Bold 11'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'BlinkMacSystemFont Semibold 11'
