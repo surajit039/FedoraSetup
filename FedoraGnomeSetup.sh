6be6fcb050b2258cec694b5336bdabf04cef1eb4
surajit039 <57254249+surajit039@users.noreply.github.com>
2026-07-30 23:16:18 +0530

    Update FedoraGnomeSetup.sh
    Added logout button for gnome-session in signle user account

--- FedoraGnomeSetup.sh
+++ FedoraGnomeSetup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 gsettings set org.gnome.desktop.wm.preferences button-layout ":maximize,close"
+gsettings set org.gnome.shell always-show-log-out true
 pkexec bash -c "
 dnf remove -y totem epiphany gnome-shell-extension-window-list gnome-shell-extension-places-menu gnome-shell-extension-background-logo gnome-contacts gnome-weather gnome-maps gnome-photos mediawriter libreoffice-calc libreoffice-writer libreoffice-impress rhythmbox &&
 dnf install -y gnome-shell-extension-dash-to-dock gnome-tweaks g4music file-roller btop fragments p7zip-plugins p7zip python make cmake automake autoconf gcc g++ gnupg seahorse wget git seahorse-nautilus nautilus-extensions &&
